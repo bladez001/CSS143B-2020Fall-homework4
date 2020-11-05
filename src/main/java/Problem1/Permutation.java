358eac75f93aab5bca3eee5d62b4167fb5852327
Timothy Muresan <muresan_timothy@yahoo.com>
2020-11-04 23:56:40 -0800

    Permutation done

--- src/main/java/Problem1/Permutation.java
+++ src/main/java/Problem1/Permutation.java
@@ -5,7 +5,48 @@ import java.util.List;
 
 public class Permutation {
     public static List<List<Integer>> permute(int[] nums) {
-        // homework
-        return new ArrayList<>(); // place holder
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (nums == null) {
+            return null;
+        }
+
+        if (nums.length == 0) {
+            List<Integer> empty = new ArrayList<>();
+            result.add(empty);
+        }
+
+        if (nums == null) {
+            return null;
+        }
+
+        permute(nums, 0, result);
+        return result;
+    }
+
+    private static void permute(int[] nums, int position, List<List<Integer>> result) {
+        if (position == nums.length-1) {
+            List<Integer> newList = new ArrayList<>();
+
+            for (int i = 0; i < nums.length; i++) {
+                newList.add(nums[i]);
+            }
+
+            result.add(newList);
+            return;
+        }
+
+        for (int i = position; i < nums.length; i++) {
+            swap(nums, i, position);
+            permute(nums, position+1, result);
+            swap(nums, i, position);
+        }
+
+    }
+
+    private static void swap(int[] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
     }
 }
