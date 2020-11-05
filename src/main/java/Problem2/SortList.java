859098c432c16c1e7b5985001e9f88bb017dafa0
Timothy Muresan <muresan_timothy@yahoo.com>
2020-11-04 23:58:53 -0800

    MergeLists done

--- src/main/java/Problem2/SortList.java
+++ src/main/java/Problem2/SortList.java
@@ -22,7 +22,20 @@ public class SortList {
     }
 
     public static ListNode mergeLists(ListNode list1, ListNode list2) {
-        // homework
-        return null;
+        if (list1 == null) {
+            return list2;
+        }
+
+        if (list2 == null) {
+            return list1;
+        }
+
+        if (list1.val < list2.val) {
+            list1.next = mergeLists(list1.next, list2);
+            return list1;
+        }
+
+        list2.next = mergeLists(list1, list2.next);
+        return list2;
     }
 }
