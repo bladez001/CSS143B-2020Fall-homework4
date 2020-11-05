7c689af63378df46bbf3760351a89f85b59a0d68
Timothy Muresan <muresan_timothy@yahoo.com>
2020-11-04 23:55:10 -0800

    BinarySearch and ReverseStack done

--- src/main/java/Problem1/BinarySearch.java
+++ src/main/java/Problem1/BinarySearch.java
@@ -5,7 +5,23 @@ public class BinarySearch {
     // Do not change signature (return type, function name, parameter types)
     // Add a helper function for recursion calls
     public static int binarySearch(int[] data, int target) {
-        // homework
-        return -1;  // place holder
+        return binarySearch (data, target, 0, data.length-1);
+    }
+
+    private static int binarySearch (int[] data, int target, int left, int right) {
+        if (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (data[mid] == target) { // check if target is present at midpoint
+                return mid;
+            }
+
+            if (data[mid] < target) { // ignore left half if target is greater
+                return binarySearch(data, target, mid+1, right);
+            }
+
+            return binarySearch(data, target, left, mid-1); // ignore right side if target is smaller
+        }
+        return -1; // target was not found.
     }
 }
--- src/main/java/Problem1/ReverseStack.java
+++ src/main/java/Problem1/ReverseStack.java
@@ -4,6 +4,21 @@ import java.util.Stack;
 
 public class ReverseStack {
     public static void reverseStack(Stack<Integer> stack) {
-        // homework
+        if(!stack.isEmpty()) {
+            int pop = stack.pop(); //pop until empty
+            reverseStack(stack);
+            reInsert(stack, pop); //re-inserts
+        }
+    }
+
+    private static void reInsert(Stack<Integer> stack, int pop) {
+        if (stack.isEmpty()) {
+            stack.push(pop);
+            return;
+        }
+
+        int newPop = stack.pop(); //remove everything in stack within this method
+        reInsert(stack, pop); //push newPop to bottom
+        stack.push(newPop); //put everything else back
     }
 }
